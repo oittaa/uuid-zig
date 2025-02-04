d80f4e3a5f4e55e09310c3a5eb138e1d57adc65d
oittaa <8972248+oittaa@users.noreply.github.com>
2025-02-04 12:21:03 +0100

    Add UUIDv1 and UUIDv6 to the benchmark (#5)

--- src/main.zig
+++ src/main.zig
@@ -3,38 +3,45 @@ const Uuid = @import("Uuid.zig");
 
 pub fn main() !void {
     var print: bool = false;
-    var number: u32 = 1_000_000;
+    var number: u64 = 1_000_000;
+    var enable_v1: bool = true;
     var enable_v4: bool = true;
+    var enable_v6: bool = true;
     var enable_v7: bool = true;
     const builtin = @import("builtin");
-    var args =
-        if (builtin.os.tag == .windows or builtin.os.tag == .wasi)
-        return
-    else
-        std.process.args();
-    _ = args.next(); // trhow away the program name
-    while (args.next()) |arg| {
-        if (std.mem.eql(u8, arg, "-p") or std.mem.eql(u8, arg, "--print")) {
-            print = true;
-        } else if (std.mem.eql(u8, arg, "--disable-v4")) {
-            enable_v4 = false;
-        } else if (std.mem.eql(u8, arg, "--disable-v7")) {
-            enable_v7 = false;
-        } else {
-            number = std.fmt.parseInt(u32, arg, 10) catch |err| switch (err) {
-                error.InvalidCharacter, error.Overflow => {
-                    const stderr = std.io.getStdErr().writer();
-                    try std.fmt.format(stderr, "Usage: [<number of UUIDs, default 1_000_000>] [--print] [--disable-v4] [--disable-v7]\n", .{});
-                    std.process.exit(1);
-                },
-            };
+    if (builtin.os.tag != .windows and builtin.os.tag != .wasi) {
+        var args = std.process.args();
+        var index: usize = 0;
+        while (args.next()) |arg| : (index += 1) {
+            if (index == 0) continue; // skip the program name
+            if (std.mem.eql(u8, arg, "-p") or std.mem.eql(u8, arg, "--print")) {
+                print = true;
+            } else if (std.mem.eql(u8, arg, "--disable-v1")) {
+                enable_v1 = false;
+            } else if (std.mem.eql(u8, arg, "--disable-v4")) {
+                enable_v4 = false;
+            } else if (std.mem.eql(u8, arg, "--disable-v6")) {
+                enable_v6 = false;
+            } else if (std.mem.eql(u8, arg, "--disable-v7")) {
+                enable_v7 = false;
+            } else {
+                number = std.fmt.parseInt(u64, arg, 10) catch |err| switch (err) {
+                    error.InvalidCharacter, error.Overflow => {
+                        const stderr = std.io.getStdErr().writer();
+                        try std.fmt.format(stderr, "Usage: [<number of UUIDs, default 1_000_000>] [--print] [--disable-v<1|4|6|7>]\n", .{});
+                        std.process.exit(1);
+                    },
+                };
+            }
         }
     }
+    if (enable_v1) try benchmrk(1, number, print);
     if (enable_v4) try benchmrk(4, number, print);
+    if (enable_v6) try benchmrk(6, number, print);
     if (enable_v7) try benchmrk(7, number, print);
 }
 
-fn benchmrk(version: u8, number: u32, print: bool) !void {
+fn benchmrk(version: u8, number: u64, print: bool) !void {
     const stderr = std.io.getStdErr().writer();
     const stdout = std.io.getStdOut().writer();
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -47,7 +54,9 @@ fn benchmrk(version: u8, number: u32, print: bool) !void {
     var i: usize = 0;
     while (i < number) : (i += 1) {
         const my_uuid: Uuid = switch (version) {
+            1 => Uuid.uuid1(),
             4 => Uuid.uuid4(),
+            6 => Uuid.uuid6(),
             7 => Uuid.uuid7(),
             else => {
                 try std.fmt.format(stderr, "ERROR: unsupported version\n", .{});
@@ -70,25 +79,14 @@ fn benchmrk(version: u8, number: u32, print: bool) !void {
     }
 }
 
-test "can generate UUIDv4 and UUIDv7" {
+test "Can generate UUIDs" {
     const testing = std.testing;
+    const uuid_v1 = Uuid.uuid1();
+    try testing.expectEqual(1, uuid_v1.version());
     const uuid_v4 = Uuid.uuid4();
     try testing.expectEqual(4, uuid_v4.version());
+    const uuid_v6 = Uuid.uuid6();
+    try testing.expectEqual(6, uuid_v6.version());
     const uuid_v7 = Uuid.uuid7();
     try testing.expectEqual(7, uuid_v7.version());
-}
-
-test "args - get an iterator, no allocation but not fully portable" {
-    const builtin = @import("builtin");
-    var args =
-        if (builtin.os.tag == .windows or builtin.os.tag == .wasi)
-        // this sample does not work on Windows and WASI
-        return
-    else
-        // Linux, MacOS etc. can use the simpler args() method:
-        std.process.args();
-
-    while (args.next()) |arg| {
-        _ = arg; // use arg
-    }
 }
